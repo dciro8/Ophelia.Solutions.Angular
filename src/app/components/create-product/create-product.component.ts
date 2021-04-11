a12f9b3b33ad448143f3d9abd821eb74dda9fb7c
David Fernando Ciro Zapata <inforfercho@gmail.com>
2021-04-11 17:16:13 -0500

    Implementacion de todos los productos en el crud

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -13,6 +13,7 @@ export class CreateProductComponent implements OnInit {
   productDto: ProductDto = {};
   
   response: ResponseMessage={};
+  ProductDto?: ProductDto[];
 
   submitted = false;
   constructor(private productAllService: ProductAllService) { }
@@ -23,25 +24,36 @@ export class CreateProductComponent implements OnInit {
 
   getRegister(): void {
 
-    this.getProductForYear();
+    this.getProductAll();
+
+    this.setCreteProduct();
   }
 
-  getProductForYear(): void {
+  setCreteProduct(): void {
     this.productAllService.setProduct(this.productDto)
       .subscribe(
-
         data => {
-
           this.response= data;
 
           if (this.response != undefined && this.response.result !=undefined && this.response.result<0 ) {
             this.submitted=true;
-           
           }
           else {
          
             this.submitted=false;
          }
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  
+  getProductAll(): void {
+    this.productAllService.getAll()
+      .subscribe(
+        data => {
+          this.ProductDto = data;
         },
         error => {
           console.log(error);
