a172f70fd02e68bc6998f295e375f9fae1ad6112
David Fernando Ciro Zapata <inforfercho@gmail.com>
2021-04-17 17:28:46 -0500

    Implementacion global de multilenguaje

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {TranslateService} from '@ngx-translate/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular 11 Crud';
+
+  constructor(private translate: TranslateService) {
+ translate.setDefaultLang('es');
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { TutorialDetailsComponent } from './components/tutorial-details/tutorial
 import {GetAllProductListComponent} from './components/ProdcutAll-list/ProdcutAll-list.component';
 import { CreateProductComponent } from './components/create-product/create-product.component';
 
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient} from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,9 +27,23 @@ import { CreateProductComponent } from './components/create-product/create-produ
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+        }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
